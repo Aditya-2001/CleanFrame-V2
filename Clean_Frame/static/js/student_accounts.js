bdf66df1d30ddcfb7b5e38593fec0d786019efc8
Aditya-2001 <aditya.iiita2001@gmail.com>
2021-07-08 09:35:58 +0530

    Disclaimer changed for manage company internships

--- Clean_Frame/static/js/student_accounts.js
+++ Clean_Frame/static/js/student_accounts.js
@@ -4,3 +4,6 @@ function call_preloader1(){
 function call_preloader2(){
     document.getElementById("para2").style.display="block"
 }
+function close_me(){
+    document.getElementById("close_me").style.display="none";
+}
